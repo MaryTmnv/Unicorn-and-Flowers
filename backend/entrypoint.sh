6ae35ab92f6b83ff8d244ac445c99d31667f3b9e
Владислав Драгушин <maxhc71@yandex.ru>
2023-10-11 12:58:52 +0300

    Added shell script

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Start makemigrations..."
+python manage.py makemigrations --dry-run --check &&
+echo "Start migrate..."
+python manage.py migrate --noinput &&
+echo "Start collectstatic..."
+python manage.py collectstatic --noinput &&
+echo "Start production server..."
+gunicorn -b 0.0.0.0:8081 flowershop.wsgi:application
